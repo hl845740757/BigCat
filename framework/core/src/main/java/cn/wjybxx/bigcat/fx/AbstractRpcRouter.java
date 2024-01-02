ea38debe13ec5644c9de77e6dbf7c7bcc4c9aa66
wjybxx <845740757@qq.com>
2024-01-02 11:14:32 +0800

    fix copyright 日期

--- framework/core/src/main/java/cn/wjybxx/bigcat/fx/AbstractRpcRouter.java
+++ framework/core/src/main/java/cn/wjybxx/bigcat/fx/AbstractRpcRouter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 wjybxx
+ *  Copyright 2023-2024 wjybxx
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- tools/tools-config/src/main/java/cn/wjybxx/bigcat/config/NormalSheetReader.java
+++ tools/tools-config/src/main/java/cn/wjybxx/bigcat/config/NormalSheetReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 wjybxx(845740757@qq.com)
+ * Copyright 2023-2024 wjybxx(845740757@qq.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
