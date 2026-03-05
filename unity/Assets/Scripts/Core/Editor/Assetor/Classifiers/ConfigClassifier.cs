056f3c660a27df9e3fa665860e821170c04a262f
wjybxx <845740757@qq.com>
2026-03-05 19:06:02 +0800

    提交遗漏文件

---
+++ unity/Assets/Scripts/Core/Editor/Assetor/Classifiers/ConfigClassifier.cs
@@ -0,0 +1,57 @@
+﻿#region LICENSE
+
+// Copyright 2026 wjybxx(845740757@qq.com)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Wjybxx.BigCat.Core;
+using Wjybxx.Dson.Codec.Attributes;
+
+namespace Wjybxx.BigCat.Editor.Assetor.Classifiers
+{
+/// <summary>
+/// 配置表分类器
+/// </summary>
+[DsonSerializable]
+public class ConfigClassifier : IAssetClassifier
+{
+    /// <summary>
+    /// 原始文件扩展名
+    /// </summary>
+    public HashSet<string> rawFileExtensions = new HashSet<string>();
+    /// <summary>
+    /// 忽略文件扩展名
+    /// </summary>
+    public HashSet<string> ignoreFileExtensions = new HashSet<string>();
+
+    public EAssetCategory GetCategory(string assetPath) {
+        string extension = UnityEditorUtil.GetExtension(assetPath);
+        if (rawFileExtensions.Contains(extension)) {
+            return EAssetCategory.RawFile;
+        }
+        if (ignoreFileExtensions.Contains(extension)) {
+            return EAssetCategory.None;
+        }
+        Object asset = AssetDatabase.LoadMainAssetAtPath(assetPath);
+        if (asset == null || asset.GetType().Namespace == "UnityEditor") {
+            return EAssetCategory.None; // 通常为文件夹和代码等
+        }
+        return EAssetCategory.MainAsset;
+    }
+}
+}
---
+++ unity/Assets/Scripts/Core/Editor/Menus/AssetMenus.cs
@@ -0,0 +1,48 @@
+﻿#region LICENSE
+
+// Copyright 2026 wjybxx(845740757@qq.com)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using UnityEditor;
+using Wjybxx.BigCat.Core;
+
+namespace Wjybxx.BigCat.Editor.Util
+{
+public static class AssetMenus
+{
+    [MenuItem("Window/BigCat/EditorMenus/RefreshSpriteGroup")]
+    public static void RefreshAllSpriteGroups() {
+        foreach (string guid in AssetDatabase.FindAssets("t:SpriteGroup", UnityEditorUtil.spriteSearchFolders)) {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            SpriteGroup spriteGroup = AssetDatabase.LoadAssetAtPath<SpriteGroup>(assetPath);
+            if (spriteGroup) {
+                spriteGroup.Refresh();
+            }
+        }
+    }
+    
+    [MenuItem("Window/BigCat/EditorMenus/RefreshAudioGroup")]
+    public static void RefreshAllAudioGroups() {
+        foreach (string guid in AssetDatabase.FindAssets("t:AudioGroup", UnityEditorUtil.audioSearchFolders)) {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            AudioGroup audioGroup = AssetDatabase.LoadAssetAtPath<AudioGroup>(assetPath);
+            if (audioGroup) {
+                audioGroup.Refresh();
+            }
+        }
+    }
+}
+}
